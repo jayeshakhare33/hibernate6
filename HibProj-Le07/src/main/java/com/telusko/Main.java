60b9203deaaedd01d8d044bee694e804b6cdc6fe
gaurav <gsharma1154@gmail.com>
2025-01-21 18:35:38 +0530

    upto le-9 code

--- HibProj-Le07/src/main/java/com/telusko/Main.java
+++ HibProj-Le07/src/main/java/com/telusko/Main.java
@@ -9,29 +9,27 @@ public class Main {
     public static void main(String[] args) {
 
         Student s1 = new Student();
-//        s1.setRollNo(102);
-//        s1.setsName("Kiran");
-//        s1.setsAge(25);
 
 
-//        s1.setRollNo(104);
-//        s1.setsName("Sushil");
-//        s1.setsAge(19);
+        s1.setRollNo(106);
+        s1.setsName("Avni");
+        s1.setsAge(21);
 
-        s1.setRollNo(105);
-        s1.setsName("Gaurav");
-        s1.setsAge(22);
 
-        Configuration cfg = new Configuration();
-        cfg.addAnnotatedClass(com.telusko.Student.class);
-        cfg.configure("hibernate.cfg.xml");
-        SessionFactory sf = cfg.buildSessionFactory();
+        SessionFactory sf = new Configuration()
+                .configure()
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
         Session session = sf.openSession();
         Transaction transaction = session.beginTransaction();
 
         session.persist(s1);
 
+
         transaction.commit();
+        session.close();
+        sf.close();
         System.out.println(s1);
 
     }
