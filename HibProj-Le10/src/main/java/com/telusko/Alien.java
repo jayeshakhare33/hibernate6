2d042d136ccac1a07595506d79474e902907cbd0
gaurav <gsharma1154@gmail.com>
2025-01-28 11:22:03 +0530

    changing column and table names

---
+++ HibProj-Le10/src/main/java/com/telusko/Alien.java
@@ -0,0 +1,43 @@
+package com.telusko;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "alien_table")
+public class Alien {
+
+    @Id
+    private int aid;
+
+    @Column(name = "alien_name")
+    private String aname;
+
+    @Transient
+    private String tech;
+
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+
+    public String getTech() {
+        return tech;
+    }
+
+    public void setTech(String tech) {
+        this.tech = tech;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+
+}
